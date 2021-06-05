ffeb791b7344f6f0b17a19f72bc839c005413b1d
loevliedenny@gmail.com <59749099+loevlie@users.noreply.github.com>
2021-06-05 16:03:16 -0400

    Adding Post Stuff

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
 from ckeditor.fields import RichTextField
-
+from django.urls import reverse
 # Create your models here.
 STATUS = (
     (0,"Draft"),
@@ -24,6 +24,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse('post_detail', kwargs={"pk": self.pk})
+    
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=(str(self.id)))
+    
 
 
 class Comment(models.Model):
